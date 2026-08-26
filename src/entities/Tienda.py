4dbbd827ad44d124320edaf9bf73f16d2d916b1e
Tu Nombre <miraospinasimon@gmail.com>
2026-08-26 18:48:03 -0500

    feat: agregar entidad tienda

---
+++ src/entities/Tienda.py
@@ -0,0 +1,7 @@
+class Tienda:
+    def __init__(self, id_tienda: int, nombre_sucursal: str, direccion: str, telefono: str, horario_apertura: str):
+        self.id_tienda = id_tienda
+        self.nombre_sucursal = nombre_sucursal
+        self.direccion = direccion
+        self.telefono = telefono
+        self.horario_apertura = horario_apertura
