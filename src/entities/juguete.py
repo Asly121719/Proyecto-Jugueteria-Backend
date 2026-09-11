048ea31244b247522edda8c6671be1de97f81841
Asly121719 <aslycastano84@gmail.com>
2026-09-11 13:41:51 -0500

    feat:modelo de la entidad juguete

--- src/entities/juguete.py
+++ src/entities/juguete.py
@@ -1,21 +1,38 @@
-from .categoria import Categoria 
+from src.database import Base
+from sqlalchemy import Column, ForeignKey, Integer, Numeric, String, Table
+from sqlalchemy.orm import relationship
 
- 
+promocion_juguetes = Table(
+    "promocion_juguetes",
+    Base.metadata,
+    Column(
+        "id_promocion",
+        Integer,
+        ForeignKey("promociones.id_promocion"),
+        primary_key=True,
+    ),
+    Column(
+        "id_juguete",
+        Integer,
+        ForeignKey("juguetes.id_juguete"),
+        primary_key=True,
+    ),
+    extend_existing=True,  # <--- Debe ir aquí, como argumento de Table, fuera de las columnas
+)
 
-class Juguete: 
 
-    def __init__(self, id_juguete: int, id_categoria: int, id_proveedor: int, nombre_producto: str, precio_unitario: float, categoria: Categoria = None): 
+class Juguete(Base):
+  _tablename_ = "juguetes"
 
-        self.id_juguete = id_juguete 
-
-        self.id_categoria = id_categoria 
-
-        self.id_proveedor = id_proveedor 
-
-        self.nombre_producto = nombre_producto 
-
-        self.precio_unitario = precio_unitario 
-
-        self.categoria = categoria  # Relación con la clase Categoria 
-
- 
+  id_juguete = Column(Integer, primary_key=True, autoincrement=True)
+  id_categoria = Column(Integer, ForeignKey("categorias.id_categoria"))
+  id_proveedor = Column(Integer, ForeignKey("proveedores.id_proveedor"))
+  nombre_producto = Column(String(150), nullable=False)
+  precio_unitario = Column(Numeric(10, 2), nullable=False)
+  detalles = relationship("DetalleVenta", back_populates="juguete")
+  categoria = relationship("Categoria", back_populates="juguetes")
+  proveedor = relationship("Proveedor", back_populates="juguetes")
+  inventarios = relationship("InventarioTienda", back_populates="juguete")
+  promociones = relationship(
+      "Promocion", secondary=promocion_juguetes, back_populates="juguetes"
+  )
