165c249b67faf5efd1ae350006451b11fec7e043
Tu Nombre <miraospinasimon@gmail.com>
2026-09-11 14:51:26 -0500

    feat: modelo de la entidad tienda

--- src/entities/Tienda.py
+++ src/entities/Tienda.py
@@ -1,7 +1,17 @@
-class Tienda:
-    def __init__(self, id_tienda: int, nombre_sucursal: str, direccion: str, telefono: str, horario_apertura: str):
-        self.id_tienda = id_tienda
-        self.nombre_sucursal = nombre_sucursal
-        self.direccion = direccion
-        self.telefono = telefono
-        self.horario_apertura = horario_apertura
+from src.database import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+
+class Tienda(Base):
+  _tablename_ = "tiendas"
+
+  id_tienda = Column(Integer, primary_key=True, autoincrement=True)
+  nombre_sucursal = Column(String(100), nullable=False)
+  direccion = Column(String(255), nullable=False)
+  telefono = Column(String(50), nullable=False)
+  horario_apertura = Column(String(100), nullable=False)
+
+  empleados = relationship("Empleado", back_populates="tienda")
+  inventarios = relationship("InventarioTienda", back_populates="tienda")
+  ventas = relationship("Venta", back_populates="tienda")
